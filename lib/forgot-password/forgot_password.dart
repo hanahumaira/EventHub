3578c1a8e3fb9a39d98d70d12ad1da124d90008a
Atifah-Din <atifah02@graduate.utm.my>
2024-04-28 12:30:51 +0800

    Update forgot_password.dart

--- lib/forgot-password/forgot_password.dart
+++ lib/forgot-password/forgot_password.dart
@@ -8,6 +8,7 @@ void main() {
 
 class ForgotPasswordPage extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -15,34 +16,51 @@ class ForgotPasswordPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Forgot Password'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                hintText: 'Enter your registered email',
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  hintText: 'Enter your registered email',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email address';
+                  }
+                  // Regular expression for email validation
+                  final emailRegExp =
+                      RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                  if (!emailRegExp.hasMatch(value)) {
+                    return 'Please enter a valid email address';
+                  }
+                  return null;
+                },
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Code to send email and show notification
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Password reset email sent'),
-                  ),
-                );
-                // Simulate redirection to mockup-gmail page
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MockupGmailPage()),
-                );
-              },
-              child: Text('Send'),
-            ),
-          ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState != null &&
+                      _formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Password reset email sent'),
+                      ),
+                    );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MockupGmailPage()),
+                    );
+                  }
+                },
+                child: Text('Send'),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -64,22 +82,19 @@ class MockupGmailPage extends StatelessWidget {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Simulate redirection to login page
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => LoginPage()),
                 );
               },
-              child: Text('Login'),
+              child: Text('Log In'),
             ),
             SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
-                // Simulate redirection to enter new password page
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) => EnterNewPasswordPage()),
+                  MaterialPageRoute(builder: (context) => ResetPasswordPage()),
                 );
               },
               child: Text('Reset Password'),
@@ -101,7 +116,6 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            // Simulate automatic login and redirection to main page
             Navigator.pushReplacement(
               context,
               MaterialPageRoute(builder: (context) => MainPage()),
@@ -114,47 +128,69 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class EnterNewPasswordPage extends StatelessWidget {
+class ResetPasswordPage extends StatelessWidget {
   final TextEditingController _newPasswordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Enter New Password'),
+        title: Text('Reset Password'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _newPasswordController,
-              decoration: InputDecoration(
-                hintText: 'Enter new password',
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _newPasswordController,
+                decoration: InputDecoration(
+                  hintText: 'Enter new password',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a new password';
+                  }
+                  return null;
+                },
               ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: _confirmPasswordController,
-              decoration: InputDecoration(
-                hintText: 'Re-enter new password',
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _confirmPasswordController,
+                decoration: InputDecoration(
+                  hintText: 'Re-enter new password',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please re-enter your new password';
+                  }
+                  if (value != _newPasswordController.text) {
+                    return 'Passwords do not match';
+                  }
+                  return null;
+                },
               ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Code to update password
-                // Redirect to login page
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-              child: Text('Submit'),
-            ),
-          ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState != null &&
+                      _formKey.currentState!.validate()) {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -174,146 +210,3 @@ class MainPage extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// // import 'package:eventhub/forgot-password/email_validator.dart';
-
-
-// void main() {
-//   runApp(MaterialApp(
-//     title: 'Reset Password Page',
-//     initialRoute: '/',
-//     routes: {
-//       '/': (context) => EmailEntryPage(),
-//       '/password': (context) => PasswordEntryPage(),
-//       '/main': (context) => MainPage(),
-//     },
-//   ));
-// }
-
-// class EmailEntryPage extends StatelessWidget {
-//   final TextEditingController _emailController = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Reset Password'),
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.all(20.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: [
-//             TextField(
-//               controller: _emailController,
-//               decoration: InputDecoration(labelText: 'Enter your email'),
-//             ),
-//             SizedBox(height: 20.0),
-//             ElevatedButton(
-//               onPressed: () {
-//                 String email = _emailController.text.trim();
-//                 var EmailValidator;
-//                 if (EmailValidator.validate(email)) {
-//                   Navigator.pushNamed(context, '/password', arguments: email);
-//                 } else {
-//                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//                     content: Text('Please enter a valid email.'),
-//                   ));
-//                 }
-//               },
-//               child: Text('Send'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class PasswordEntryPage extends StatelessWidget {
-//   final TextEditingController _passwordController = TextEditingController();
-//   final TextEditingController _confirmPasswordController =
-//       TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final String email = ModalRoute.of(context)?.settings.arguments as String;
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Reset Password'),
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.all(20.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: [
-//             Text('Reset Password for: $email'),
-//             SizedBox(height: 20.0),
-//             TextField(
-//               controller: _passwordController,
-//               decoration: InputDecoration(labelText: 'New Password'),
-//               obscureText: true,
-//             ),
-//             SizedBox(height: 10.0),
-//             TextField(
-//               controller: _confirmPasswordController,
-//               decoration: InputDecoration(labelText: 'Confirm New Password'),
-//               obscureText: true,
-//             ),
-//             SizedBox(height: 20.0),
-//             ElevatedButton(
-//               onPressed: () {
-//                 // Check password and confirm password match
-//                 if (_passwordController.text ==
-//                     _confirmPasswordController.text) {
-//                   _showPasswordResetSuccess(context);
-//                 } else {
-//                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-//                     content: Text('Passwords do not match.'),
-//                   ));
-//                 }
-//               },
-//               child: Text('Reset'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   void _showPasswordResetSuccess(BuildContext context) {
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return AlertDialog(
-//           title: Text('Password Reset Successful'),
-//           content: Text('Your password has been successfully reset.'),
-//           actions: [
-//             TextButton(
-//               onPressed: () {
-//                 Navigator.popUntil(context, ModalRoute.withName('/'));
-//               },
-//               child: Text('OK'),
-//             ),
-//           ],
-//         );
-//       },
-//     );
-//   }
-// }
-
-// class MainPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Main Page'),
-//       ),
-//       body: Center(
-//         child: Text('Welcome to the Main Page!'),
-//       ),
-//     );
-//   }
-// }
